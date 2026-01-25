b511849160c5e9dd21b8997adfee0de3ac1691dd
Michaël AUGUSTE <github.ej675@passmail.net>
2026-01-25 18:57:18 +0100

    passage version 1.2.0

--- py_convert/__init__.py
+++
@@ -1,3 +0,0 @@
-from .settings_manager import Settings
-from .error import run_error
-from .cli import main
--- py_convert/gui/__init__.py
+++
@@ -1,7 +0,0 @@
-from .app import App
-from .main import MyFrame
-from .menu import MenuBar
-from .settings import SettingsWindow
-from .about import AboutWindow
-from .cash import AskCash
-from .animated_toggle import QSwitch
